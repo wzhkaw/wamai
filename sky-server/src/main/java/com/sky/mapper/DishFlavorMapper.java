980177b0b2ca84b7d896c88a07ba6b8bf0b5e974
wzhkaw <1464869055@qq.com>
2025-11-07 22:03:59 +0800

    新增菜品业务功能开发

---
+++ sky-server/src/main/java/com/sky/mapper/DishFlavorMapper.java
@@ -0,0 +1,18 @@
+package com.sky.mapper;
+
+import com.sky.annotation.AutoFill;
+import com.sky.entity.DishFlavor;
+import com.sky.enumeration.OperationType;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface DishFlavorMapper {
+
+    /*
+    * 批量插入口味
+    * */
+//    @AutoFill(value = OperationType.INSERT)
+    void insertBatch(List<DishFlavor> flavors);
+}
