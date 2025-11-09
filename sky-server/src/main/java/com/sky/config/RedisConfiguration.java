7fd9812bf2f8c117a42470df3df0dbcf776ef3c1
wzhkaw <1464869055@qq.com>
2025-11-09 15:29:39 +0800

    缓存套餐业务功能开发

--- sky-server/src/main/java/com/sky/config/RedisConfiguration.java
+++ sky-server/src/main/java/com/sky/config/RedisConfiguration.java
@@ -1,15 +1,18 @@
 package com.sky.config;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 @Slf4j
 public class RedisConfiguration {
 
+    @Bean
     public RedisTemplate redisTemplate(RedisConnectionFactory redisConnectionFactory){
         log.info("开始创建redis模板对象...");
         RedisTemplate redisTemplate = new RedisTemplate();
@@ -17,6 +20,18 @@ public class RedisConfiguration {
         redisTemplate.setConnectionFactory(redisConnectionFactory);
         //设置redis key的序列化器
         redisTemplate.setKeySerializer(new StringRedisSerializer());
+
+//        //后面是解决键在redis中乱码问ai加上的
+//
+//        // 添加值的序列化器 - 使用JSON格式
+//        Jackson2JsonRedisSerializer<Object> serializer = new Jackson2JsonRedisSerializer<>(Object.class);
+//        redisTemplate.setValueSerializer(serializer);
+//
+//        // 如果需要操作Hash类型，也设置序列化器
+//        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+//        redisTemplate.setHashValueSerializer(serializer);
+//
+//        redisTemplate.afterPropertiesSet();
         return redisTemplate;
     }
 }
--- sky-server/src/main/java/com/sky/controller/admin/DishController.java
+++ sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -11,9 +11,11 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Set;
 
 
 /*
@@ -28,6 +30,9 @@ public class DishController {
     @Autowired
     private DishService dishService;
 
+    @Autowired
+    private RedisTemplate redisTemplate;
+
     /*
     * 新增菜品
     * */
@@ -36,6 +41,9 @@ public class DishController {
     public Result save(@RequestBody DishDTO dishDTO){
         log.info("新增菜品:{}",dishDTO);
         dishService.saveWithFlavor(dishDTO);
+
+        //清理缓存
+        cleanCache("*dish_"+dishDTO.getCategoryId());
         return Result.success();
     }
 
@@ -58,6 +66,10 @@ public class DishController {
     public Result delete(@RequestParam List<Long> ids){
         log.info("菜品批量删除:{}",ids);
         dishService.deleteBatch(ids);
+
+        //将所有开头为dish_的缓存全删了
+        cleanCache("*dish_*");
+
         return Result.success();
     }
 
@@ -80,6 +92,9 @@ public class DishController {
     public Result update(@RequestBody DishDTO dishDTO){
         log.info("修改菜品:{}",dishDTO);
         dishService.updateWithFlavor(dishDTO);
+
+        cleanCache("*dish_*");
+
         return Result.success();
     }
 
@@ -96,10 +111,14 @@ public class DishController {
         return Result.success(list);
     }
 
-
-
-
-
+    /*
+    * 清理缓存
+    * */
+    private void cleanCache(String pattern) {
+        //将所有开头为dish_的缓存全删了
+        Set keys = redisTemplate.keys(pattern);
+        redisTemplate.delete(keys);
+    }
 
 
 
